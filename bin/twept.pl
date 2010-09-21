13d9a23e1d1f9b6a553ef3f8fcba68d6c74ebe84
Twinkle Computing <twinkle@cpan.org>
2010-09-21 22:23:38 +0000

    Import of TWINKLE/HTML-EmbeddedPerl-Test-0.05 from CPAN.
    gitpan-cpan-distribution: HTML-EmbeddedPerl-Test
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         TWINKLE/HTML-EmbeddedPerl-Test-0.05.tar.gz
    gitpan-cpan-author:       TWINKLE
    gitpan-cpan-maturity:     released

--- bin/twept.pl
+++ bin/twept.pl
@@ -2,7 +2,7 @@
 package twept;
 use strict;
 use warnings;
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 our $TIMEOUT = 2;
 local $SIG{ALRM} = sub{ die 'Force exiting, detected loop'; };
 my $STDBAK = *STDOUT;
--- lib/HTML/EmbeddedPerl/Test.pm
+++ lib/HTML/EmbeddedPerl/Test.pm
@@ -9,7 +9,7 @@ our @ISA       = qw(Exporter);
 our @EXPORT    = qw(ep);
 our @EXPORT_OK = qw($VERSION $TIMEOUT);
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 our $TIMEOUT = 2;
 
 my $STDBAK = *STDOUT;
